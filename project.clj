dad48d59fdb944f32f1069055fe6fbfc9b5f8cb0
James McMahon <git.spamfriedrice@spamgourmet.com>
2019-01-20 16:45:21 -0600

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject play-clj "1.2.0-SNAPSHOT"
+(defproject play-clj "1.2.1-SNAPSHOT"
   :description "A libGDX wrapper for easy cross-platform game development"
   :url "https://github.com/oakes/play-clj"
   :license {:name "Public Domain"
             :url "http://unlicense.org/UNLICENSE"}
-  :dependencies [[com.badlogicgames.gdx/gdx "1.9.3"]
-                 [com.badlogicgames.gdx/gdx-box2d "1.9.3"]
-                 [com.badlogicgames.gdx/gdx-bullet "1.9.3"]
-                 [org.clojure/clojure "1.7.0"]]
+  :dependencies [[com.badlogicgames.gdx/gdx "1.9.9"]
+                 [com.badlogicgames.gdx/gdx-box2d "1.9.9"]
+                 [com.badlogicgames.gdx/gdx-bullet "1.9.9"]
+                 [org.clojure/clojure "1.10.0"]]
   :repositories [["sonatype"
                   "https://oss.sonatype.org/content/repositories/releases/"]]
   :source-paths ["src"]
   :java-source-paths ["src-java"]
-  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options"])
+  :javac-options ["-target" "1.8" "-source" "1.8" "-Xlint:-options"])
